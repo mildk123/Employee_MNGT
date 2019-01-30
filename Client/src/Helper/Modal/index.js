744a802160af4091f1172d8322127cf416041a4f
Milad Khan <mildk123@gmail.com>
2019-01-31 03:37:58 +0500

    added auth ui

--- Client/src/Helper/Modal/index.js
+++ Client/src/Helper/Modal/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { Button, Modal, Icon } from 'semantic-ui-react'
 import { Input } from 'semantic-ui-react'
 import { Grid } from 'semantic-ui-react'
-
+import swal from 'sweetalert'
 
 class AuthModal extends Component {
   constructor() {
@@ -12,14 +12,55 @@ class AuthModal extends Component {
 
   show = () => this.setState({ open: true })
   close = () => this.setState({ open: false })
+  onChangeHandler = (name, value) => {
+    this.setState({
+      [name]: value
+    })
+  }
+  action = () => {
+    const { email, password } = this.state;
+    if (!email || !password) {
+      swal('Invalid Email/Password')
+    } else {
+      if (this.props.type === 'Register') {
+        fetch('/auth/register', {
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            email: email,
+            password: password,
+          })
+        }
+        )
+          .then(data => data.json())
+          .then(dat => swal(dat))
+          .catch(err => swal(err.message))
+
+      } else {
+        fetch('/auth/login', {
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ email: email, password: password })
+        }
+        )
+          .then(data => data.json())
+          .then(dat => swal(dat))
+          .catch(err => swal(err.message))
+      }
+    }
+  }
 
   render() {
     const { open } = this.state
 
     return (
       <div>
-        {/* <Button onClick={this.show('mini')}>Mini</Button> */}
-
         <Modal size='tiny' open={open} onClose={this.close}>
           <Modal.Header>{this.props.modalTitle}</Modal.Header>
 
@@ -27,8 +68,9 @@ class AuthModal extends Component {
             <Grid>
 
               <Grid.Row columns={1}>
-                <Grid.Column width={15}>
+                <Grid.Column >
                   <Input
+                    onChange={(text) => { this.onChangeHandler('email', text.target.value) }}
                     tabIndex="1"
                     fluid
                     label={<Icon name='mail' />}
@@ -40,8 +82,9 @@ class AuthModal extends Component {
               </Grid.Row>
 
               <Grid.Row columns={1}>
-                <Grid.Column width={15}>
+                <Grid.Column >
                   <Input
+                    onChange={(text) => { this.onChangeHandler('password', text.target.value) }}
                     tabIndex="2"
                     fluid
                     label={<Icon name='key' />}
@@ -58,9 +101,9 @@ class AuthModal extends Component {
           </Modal.Content>
 
           <Modal.Actions>
-            <Button negative>Cancel</Button>
+            <Button negative onClick={this.close}>Cancel</Button>
 
-            <Button color='black' icon labelPosition='right'>
+            <Button color='black' icon labelPosition='right' onClick={this.action}>
               {this.props.type}
               <Icon name={this.props.btnIcon} />
             </Button>
