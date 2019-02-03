e825086b8eea4d1239ab5fcdc08a6186f2f26045
Milad Khan <mildk123@gmail.com>
2019-02-03 21:15:32 +0500

    added sign out

--- Client/src/Helper/Appbar/index.js
+++ Client/src/Helper/Appbar/index.js
@@ -4,51 +4,77 @@ import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import { Button, Icon } from 'semantic-ui-react'
 
-
-import { Icon } from "semantic-ui-react";
-import { IconButton, Button } from "@material-ui/core";
 
 const styles = {
     root: {
         flexGrow: 1,
     },
+    grow: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginLeft: -12,
+        marginRight: 20,
+    },
 };
 
+class MenuAppBar extends React.Component {
+    state = {
+        auth: true,
+        anchorEl: null,
+    };
+
+    render() {
+        const { classes } = this.props;
+        const { auth, anchorEl } = this.state;
+        const open = Boolean(anchorEl);
+
+        return (
+            <div className={classes.root}>
+
+                <AppBar position="static" style={{ background: ' linear-gradient(10deg, #1D976C, #93F9B9)' }}>
+                    <Toolbar>
+                        <IconButton>
+                            <Icon
+                                className="styles.icon"
+                                name="users"
+                                size='large'
+                                inverted
+                            />
+                        </IconButton>
+                        <Typography variant="h6" color="inherit" className={classes.grow}>
+                            Home
+            </Typography>
+
+                        <div>
+                            <Button
+                                onClick={() => {
+                                    sessionStorage.removeItem('SessionToken')
+                                    this.props.history.replace('/')
+                                }}
+                                animated color='twitter' >
+                                <Button.Content visible>Logout</Button.Content>
+                                <Button.Content hidden>
+                                    <Icon name='sign-out' />
+                                </Button.Content>
+                            </Button>
 
 
-function SimpleAppBar(props) {
-    const { classes } = props;
+                        </div>
 
-    return (
-        <div className={classes.root}>
-            <AppBar position="static" style={{ background: ' linear-gradient(10deg, #1D976C, #93F9B9)' }}>
-                <Toolbar>
-                    <IconButton>
-                        <Icon
-                            className="styles.icon"
-                            name="users"
-                            size='large'
-                            inverted
-                        />
-                    </IconButton>
-                    <Typography variant={props.variant} color={props.textColor}>
-                        {props.children}
-                    </Typography>
-                    <Button
-                        onClick={() => {
-                            sessionStorage.removeItem('SessionToken')
-                            props.history.replace('/')
-                        }}
-                    >Sign Out</Button>
-                </Toolbar>
-            </AppBar>
-        </div>
-    );
+                    </Toolbar>
+                </AppBar>
+            </div>
+        );
+    }
 }
 
-SimpleAppBar.propTypes = {
+MenuAppBar.propTypes = {
     classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(SimpleAppBar);
+export default withStyles(styles)(MenuAppBar);
+
