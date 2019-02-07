b6f6e7121cfed81c8b8f6ad904cd119ffd82ee25
Milad Khan <mildk123@gmail.com>
2019-02-07 20:41:01 +0500

    heroku

---
+++ Client/build/precache-manifest.65e5c2b2102c1047467711b2bfea8dab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a9c91f7e928334f7b76b",
+    "url": "/static/js/main.a9c91f7e.chunk.js"
+  },
+  {
+    "revision": "127e831b230b43675019",
+    "url": "/static/js/1.127e831b.chunk.js"
+  },
+  {
+    "revision": "a9c91f7e928334f7b76b",
+    "url": "/static/css/main.5918bc71.chunk.css"
+  },
+  {
+    "revision": "1ef8d5478f4a19c2a91b6c7e429bd220",
+    "url": "/index.html"
+  }
+];
